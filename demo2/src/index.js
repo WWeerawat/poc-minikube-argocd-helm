31344d53a42ee66e5b12b3ab948f9d76d760ad91
Weerawat <weerwat@primo.mobi>
2024-04-28 16:43:32 +0700

    update to 1.0.3

--- demo2/src/index.js
+++ demo2/src/index.js
@@ -2,73 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const k8s = require('@kubernetes/client-node');
-const kc = new k8s.KubeConfig();
-kc.loadFromDefault();
+// Read package.json to get version
+const { version } = require('./package.json');
 
-const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
-const k8sCoreApi = kc.makeApiClient(k8s.CoreV1Api);
+app.get('/', (req, res) => {
+  // Get pod name from environment variable (if running in Kubernetes)
+  const podName = process.env.HOSTNAME || 'localhost';
 
-async function getArgoCDApplications() {
-  try {
-    const response = await k8sApi.listNamespacedDeployment('argocd');
-    const deployments = response.body.items;
-    console.log('ArgoCD Deployments:');
-    console.log(deployments);
-    return deployments;
-  } catch (error) {
-    console.error('Error getting ArgoCD deployments:', error);
-    throw error;
-  }
-}
+  // Log pod name and version
+  console.log(`Pod Name: ${podName}`);
+  console.log(`Version: ${version}`);
 
-async function getServices(namespace) {
-  try {
-    const response = await k8sCoreApi.listNamespacedService(
-      namespace
-    );
-    const services = response.body.items;
-    console.log(`Services in namespace ${namespace}:`);
-    console.log(services);
-    return services;
-  } catch (error) {
-    console.error(
-      `Error getting services in namespace ${namespace}:`,
-      error
-    );
-    throw error;
-  }
-}
-
-async function getDeployments(namespace) {
-  try {
-    const response = await k8sApi.listNamespacedDeployment(namespace);
-    const deployments = response.body.items;
-    console.log(`Deployments in namespace ${namespace}:`);
-    console.log(deployments);
-    return deployments;
-  } catch (error) {
-    console.error(
-      `Error getting deployments in namespace ${namespace}:`,
-      error
-    );
-    throw error;
-  }
-}
-
-app.get('/', async (req, res) => {
   res.send('Hello, World!');
-
-  try {
-    // Fetch and log ArgoCD applications
-    await getArgoCDApplications();
-
-    // Fetch and log services and deployments in the default namespace
-    await getServices('default');
-    await getDeployments('default');
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
-  }
 });
 
 app.listen(port, () => {

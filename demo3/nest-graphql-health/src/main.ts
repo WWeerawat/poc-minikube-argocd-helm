25a2012fc0a98c9424ac9e41a836381bd2be764c
Weerawat <weerawat190943@gmail.com>
2024-08-03 23:01:41 +0700

    manual handle signal

--- demo3/nest-graphql-health/src/main.ts
+++ demo3/nest-graphql-health/src/main.ts
@@ -5,7 +5,15 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableShutdownHooks();
+  const signals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM'];
+
+  signals.forEach((signal) => {
+    process.on(signal, async () => {
+      this.logger.log(`Received ${signal}. Shutting down gracefully...`);
+      await server.close();
+      process.exit(0);
+    });
+  });
 
   await app.listen(3000);
   Logger.log(`Application is running on: ${await app.getUrl()}`);
